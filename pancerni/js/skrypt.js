e7a562c453c69e14e43c4e5ae002b859969ffc4f
Krystian Niewiadomski <krysbul12@gmail.com>
2020-02-22 18:15:31 +0100

    up

---
+++ pancerni/js/skrypt.js
@@ -0,0 +1,18 @@
+//Przewijanie target ID
+
+$(document).ready(function() { 
+
+    $('a[href^="#"]').on('click', function(event) {
+
+    var target = $( $(this).attr('href') );
+
+    if( target.length ) {
+       event.preventDefault();
+       $('html, body').animate({
+       scrollTop: target.offset().top
+   }, 800);
+   }
+   });
+});
+
+//Przewijanie target ID
