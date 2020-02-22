65975d365ff7a329fe2bdee732c412fc599fc329
Krystian Niewiadomski <krysbul12@gmail.com>
2020-02-22 09:44:24 +0100

    Js

---
+++ java_script/js/2-variables.js
@@ -0,0 +1,15 @@
+'use strict';
+
+let name;
+let ladyName = "Kaja";
+let age = 37;
+
+let color = "czerwony";
+
+name = "Sergio";
+
+name = ladyName;
+
+console.log(name);
+console.log(color);
+console.log(age);
